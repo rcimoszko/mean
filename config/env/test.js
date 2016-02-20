ec1120badc1fae3a799c5eb23d374c3bf4043444
rcimoszko <ryan.cimoszko@gmail.com>
2016-02-20 13:22:59 -0700

    added test config variables

--- config/env/test.js
+++ config/env/test.js
@@ -3,103 +3,115 @@
 var defaultEnvConfig = require('./default');
 
 module.exports = {
-  db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/fu4-test',
-    options: {
-      user: '',
-      pass: ''
+    db: {
+        uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/fu4-test',
+        options: {
+            user: '',
+            pass: ''
+        },
+        // Enable mongoose debug mode
+        debug: process.env.MONGODB_DEBUG || false
     },
-    // Enable mongoose debug mode
-    debug: process.env.MONGODB_DEBUG || false
-  },
-  log: {
-    // logging with Morgan - https://github.com/expressjs/morgan
-    // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
-    format: process.env.LOG_FORMAT || 'combined',
-    options: {
-      // Stream defaults to process.stdout
-      // Uncomment/comment to toggle the logging to a log on the file system
-      stream: {
-        directoryPath: process.cwd(),
-        fileName: 'access.log',
-        rotatingLogs: { // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
-          active: false, // activate to use rotating logs 
-          fileName: 'access-%DATE%.log', // if rotating logs are active, this fileName setting will be used
-          frequency: 'daily',
-          verbose: false
+    log: {
+        // logging with Morgan - https://github.com/expressjs/morgan
+        // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
+        format: process.env.LOG_FORMAT || 'combined',
+        options: {
+            // Stream defaults to process.stdout
+            // Uncomment/comment to toggle the logging to a log on the file system
+            stream: {
+                directoryPath: process.cwd(),
+                fileName: 'access.log',
+                rotatingLogs: { // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
+                    active: false, // activate to use rotating logs
+                    fileName: 'access-%DATE%.log', // if rotating logs are active, this fileName setting will be used
+                    frequency: 'daily',
+                    verbose: false
+                }
+            }
         }
-      }
+    },
+    port: process.env.PORT || 3001,
+    app: {
+        title: defaultEnvConfig.app.title + ' - Test Environment'
+    },
+    facebook: {
+        clientID: process.env.FACEBOOK_ID || '208601072671209',
+        clientSecret: process.env.FACEBOOK_SECRET || '4bd32fdd07b1481066905d730d20d625',
+        callbackURL: 'https://fansunite.com/auth/facebook/callback'
+    },
+    twitter: {
+        clientID: process.env.TWITTER_KEY || 'AEcenyobBLBhPRhYCscaw',
+        clientSecret: process.env.TWITTER_SECRET || 'mwiBG42jVbjxtaVx7MQBFsUXfA1RvJyzPfL2EJQpXQU',
+        callbackURL: 'https://fansunite.com/auth/twitter/callback'
+    },
+    google: {
+        clientID: process.env.GOOGLE_ID || '499181171419-q96qpde2lldurcm0th238tdlpefibiop.apps.googleusercontent.com',
+        clientSecret: process.env.GOOGLE_SECRET || 'xaQUkh_qnPKvW7OPCszj-Adf',
+        callbackURL: 'https://fansunite.com/auth/google/callback'
+    },
+    mailer: {
+        from: process.env.MAILER_FROM || 'FansUnite <info@fansunite.com>',
+        options: {
+            service: process.env.MAILER_SERVICE_PROVIDER || 'Gmail',
+            auth: {
+                user: process.env.MAILER_EMAIL_ID || 'info@fansunite.com',
+                pass: process.env.MAILER_PASSWORD || 'Mokadoggie5'
+            }
+        }
+    },
+    linkedin: {
+        clientID: process.env.LINKEDIN_ID || 'APP_ID',
+        clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
+        callbackURL: '/api/auth/linkedin/callback'
+    },
+    github: {
+        clientID: process.env.GITHUB_ID || 'APP_ID',
+        clientSecret: process.env.GITHUB_SECRET || 'APP_SECRET',
+        callbackURL: '/api/auth/github/callback'
+    },
+    paypal: {
+        clientID: process.env.PAYPAL_ID || 'CLIENT_ID',
+        clientSecret: process.env.PAYPAL_SECRET || 'CLIENT_SECRET',
+        callbackURL: '/api/auth/paypal/callback',
+        sandbox: true
+    },
+    seedDB: {
+        seed: process.env.MONGO_SEED === 'true' ? true : false,
+        options: {
+            logResults: process.env.MONGO_SEED_LOG_RESULTS === 'false' ? false : true,
+            seedUser: {
+                username: process.env.MONGO_SEED_USER_USERNAME || 'user',
+                provider: 'local',
+                email: process.env.MONGO_SEED_USER_EMAIL || 'user@localhost.com',
+                firstName: 'User',
+                lastName: 'Local',
+                displayName: 'User Local',
+                roles: ['user']
+            },
+            seedAdmin: {
+                username: process.env.MONGO_SEED_ADMIN_USERNAME || 'admin',
+                provider: 'local',
+                email: process.env.MONGO_SEED_ADMIN_EMAIL || 'admin@localhost.com',
+                firstName: 'Admin',
+                lastName: 'Local',
+                displayName: 'Admin Local',
+                roles: ['user', 'admin']
+            }
+        }
+    },
+    // This config is set to true during grunt coverage
+    coverage: process.env.COVERAGE || false,
+    cloudinary: {
+        cloud_name: 'hltkmtrz5',
+        api_key: '289275333314649',
+        api_secret: '0BT87fZny1zaza-OQd3nAbDhAyE'
+    },
+    mailchimp: {
+        apiKey: '65e7a9d6f2340ff0a32b6108343bf392-us6',
+        listName:  'FansUnite Users'
+    },
+    stripe: {
+        secretKey: 'sk_test_vx0Z2pGXL2ROQIVv6YVrFpP1'
     }
-  },
-  port: process.env.PORT || 3001,
-  app: {
-    title: defaultEnvConfig.app.title + ' - Test Environment'
-  },
-  facebook: {
-    clientID: process.env.FACEBOOK_ID || 'APP_ID',
-    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/facebook/callback'
-  },
-  twitter: {
-    clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
-    clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
-    callbackURL: '/api/auth/twitter/callback'
-  },
-  google: {
-    clientID: process.env.GOOGLE_ID || 'APP_ID',
-    clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/google/callback'
-  },
-  linkedin: {
-    clientID: process.env.LINKEDIN_ID || 'APP_ID',
-    clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/linkedin/callback'
-  },
-  github: {
-    clientID: process.env.GITHUB_ID || 'APP_ID',
-    clientSecret: process.env.GITHUB_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/github/callback'
-  },
-  paypal: {
-    clientID: process.env.PAYPAL_ID || 'CLIENT_ID',
-    clientSecret: process.env.PAYPAL_SECRET || 'CLIENT_SECRET',
-    callbackURL: '/api/auth/paypal/callback',
-    sandbox: true
-  },
-  mailer: {
-    from: process.env.MAILER_FROM || 'MAILER_FROM',
-    options: {
-      service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
-      auth: {
-        user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
-        pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
-      }
-    }
-  },
-  seedDB: {
-    seed: process.env.MONGO_SEED === 'true' ? true : false,
-    options: {
-      logResults: process.env.MONGO_SEED_LOG_RESULTS === 'false' ? false : true,
-      seedUser: {
-        username: process.env.MONGO_SEED_USER_USERNAME || 'user',
-        provider: 'local',
-        email: process.env.MONGO_SEED_USER_EMAIL || 'user@localhost.com',
-        firstName: 'User',
-        lastName: 'Local',
-        displayName: 'User Local',
-        roles: ['user']
-      },
-      seedAdmin: {
-        username: process.env.MONGO_SEED_ADMIN_USERNAME || 'admin',
-        provider: 'local',
-        email: process.env.MONGO_SEED_ADMIN_EMAIL || 'admin@localhost.com',
-        firstName: 'Admin',
-        lastName: 'Local',
-        displayName: 'Admin Local',
-        roles: ['user', 'admin']
-      }
-    }
-  },
-  // This config is set to true during grunt coverage
-  coverage: process.env.COVERAGE || false
 };
