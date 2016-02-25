1291a1e9a9c96ffaf203ec110a3c6f6bc38dd685
rcimoszko <ryan.cimoszko@gmail.com>
2016-02-24 17:04:00 -0700

    group bets

--- modules/fu/server/bl/makepicks.server.bl.js
+++ modules/fu/server/bl/makepicks.server.bl.js
@@ -245,7 +245,7 @@ function getPicks(query, callback){
     function groupEventsByDay(callback){
         var eventsByDate = _.groupBy(processedEvents, function(event){
             var date = new Date(event.startTime);
-            date.setHours(date.getHours());
+            date.setHours(date.getHours()+TimezoneBl.timezoneAdjust);
             return new Date(date.getFullYear(), date.getMonth(), date.getDate());
         });
 
