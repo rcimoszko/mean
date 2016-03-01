6749e1ad8c3d65f2af7bc6606a64ecabf132d33a
rcimoszko <ryan.cimoszko@gmail.com>
2016-03-01 10:57:42 -0700

    update test variables

--- config/assets/test.js
+++ config/assets/test.js
@@ -2,12 +2,12 @@
 
 module.exports = {
     client: {
-        css: 'public/dist/application.min.css',
-        js: 'public/dist/application.min.js'
+        css: 'modules/dist/application.min.css',
+        js: 'modules/dist/application.min.js'
     },
-  tests: {
-    client: ['modules/*/tests/client/**/*.js'],
-    server: ['modules/*/tests/server/**/*.js'],
-    e2e: ['modules/*/tests/e2e/**/*.js']
-  }
+    tests: {
+        client: ['modules/*/tests/client/**/*.js'],
+        server: ['modules/*/tests/server/**/*.js'],
+        e2e: ['modules/*/tests/e2e/**/*.js']
+    }
 };
