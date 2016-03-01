088c44c87d9f166ca068f3a1c1f41d9e6cf32fce
rcimoszko <ryan.cimoszko@gmail.com>
2016-03-01 11:25:40 -0700

    updated test variables

--- config/assets/test.js
+++ config/assets/test.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = {
-    client: {
-        css: 'public/dist/application.min.css',
-        js: 'public/dist/application.min.js'
-    },
     tests: {
         client: ['modules/*/tests/client/**/*.js'],
         server: ['modules/*/tests/server/**/*.js'],
         e2e: ['modules/*/tests/e2e/**/*.js']
+    },
+    client: {
+        css: 'public/dist/application.min.css',
+        js: 'public/dist/application.min.js'
     }
 };
