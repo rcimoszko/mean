306342426415d84f98616087d7eb78ccfc9a29c2
rcimoszko <ryan.cimoszko@gmail.com>
2016-03-01 09:27:29 -0700

    update config

--- config/env/production.js
+++ config/env/production.js
@@ -13,8 +13,8 @@ module.exports = {
     db: {
         uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/fu4-production',
         options: {
-            user: 'heroku',
-            pass: 'Montana16'
+            user: '',
+            pass: ''
         },
         // Enable mongoose debug mode
         debug: process.env.MONGODB_DEBUG || false
