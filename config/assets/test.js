0eca445337fb109652253402ff0a38a4559a9912
rcimoszko <ryan.cimoszko@gmail.com>
2016-03-01 10:52:11 -0700

    update test variables

--- config/assets/test.js
+++ config/assets/test.js
@@ -1,6 +1,10 @@
 'use strict';
 
 module.exports = {
+    client: {
+        css: 'public/dist/application.min.css',
+        js: 'public/dist/application.min.js'
+    },
   tests: {
     client: ['modules/*/tests/client/**/*.js'],
     server: ['modules/*/tests/server/**/*.js'],
