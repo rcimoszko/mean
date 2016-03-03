dd1c0953217bbcb208374ff76bf3747253185542
rcimoszko <ryan.cimoszko@gmail.com>
2016-03-03 11:07:40 -0700

    update hockey scores

--- modules/fu/server/bl/pick.resolve.server.bl.js
+++ modules/fu/server/bl/pick.resolve.server.bl.js
@@ -22,7 +22,7 @@ function getScores(event, pick, contestantNo, opponentNo, callback){
             switch(event.sport.name.toLowerCase()){
                 case 'soccer':
                 case 'handball':
-                case 'hockey';
+                case 'hockey':
                     contestantScore = event['contestant'+contestantNo+'RegulationScore'];
                     opponentScore = event['contestant'+opponentNo+'RegulationScore'];
                     break;
