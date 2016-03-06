5ff657bf2383cdd7d0af18f01e4797b881eb4654
rcimoszko <ryan.cimoszko@gmail.com>
2016-03-06 12:11:02 -0700

    check resolve min

--- modules/fu/server/services/resolve.server.service.js
+++ modules/fu/server/services/resolve.server.service.js
@@ -12,6 +12,7 @@ function checkEventTime(event, callback){
     var diffMs = (now - eventStartTime);
     var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
     var noResolve = false;
+    console.log(diffMins);
 
     switch (event.sport.name){
         case 'Basketball':
