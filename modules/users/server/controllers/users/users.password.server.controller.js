e1785599b5d8041104fc0a74c79d5d7481ee9a1c
rcimoszko <ryan.cimoszko@gmail.com>
2016-03-07 10:14:33 -0700

    update forgot password

--- modules/users/server/controllers/users/users.password.server.controller.js
+++ modules/users/server/controllers/users/users.password.server.controller.js
@@ -30,7 +30,7 @@ exports.forgot = function (req, res, next) {
         function (token, done) {
             if (req.body.username) {
                 User.findOne({
-                    username: req.body.username.toLowerCase()
+                    username:{ $regex: new RegExp('^' + req.body.username +'$', 'i')}
                 }, '-salt -password', function (err, user) {
                     if (!user) {
                         return res.status(400).send({
