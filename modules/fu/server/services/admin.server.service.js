630a850148bedea2f7f59102652c291a2d1b8866
rcimoszko <ryan.cimoszko@gmail.com>
2016-03-09 13:31:12 -0700

    update date for check trial

--- modules/fu/server/services/admin.server.service.js
+++ modules/fu/server/services/admin.server.service.js
@@ -434,7 +434,7 @@ function checkTrial(callback){
         startDate.setDate(startDate.getDate()-7);
 
         console.log(startDate);
-        var query = {trialStartDate: {$lte:startDate}, trial:true};
+        var query = {trialStartDate: {$lte: new Date(startDate)}, trial:true};
         console.log(query);
         UserBl.getByQuery(query, callback);
     }
