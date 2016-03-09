45703e95356692ab4a51c3f16326339b65be5c85
rcimoszko <ryan.cimoszko@gmail.com>
2016-03-09 14:01:58 -0700

    add start date to user query

--- modules/fu/server/services/admin.server.service.js
+++ modules/fu/server/services/admin.server.service.js
@@ -432,6 +432,7 @@ function checkTrial(callback){
     function getTrialUsers(callback){
         var startDate = new Date();
         startDate.setDate(startDate.getDate()-7);
+        console.log(startDate);
         var query = {trialStartDate: {$lte:startDate}, trial:true};
         UserBl.getByQuery(query, callback);
     }
