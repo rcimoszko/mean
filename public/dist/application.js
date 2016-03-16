880d47135a029b73859c38dbc06b0e15384f2824
rcimoszko <ryan.cimoszko@gmail.com>
2016-03-16 14:02:13 -0600

    update logo sizes

--- public/dist/application.js
+++ public/dist/application.js
@@ -5660,6 +5660,26 @@ angular.module('fu').directive('logo', function () {
         },
         templateUrl: 'modules/fu/client/templates/contestant/logo.client.template.html',
         controller: ['$scope',  function ($scope){
+            var insert = '';
+            var subString = $scope.url.substring(0, $scope.url.lastIndexOf('/'));
+            var index = subString.lastIndexOf('/');
+
+            switch($scope.size){
+                case 'xs':
+                    insert = '/c_fit,h_25/f_auto';
+                    break;
+                case 'sm':
+                    insert = '/c_fit,h_30/f_auto';
+                    break;
+                case 'md':
+                    insert = '/c_fit,h_60/f_auto';
+                    break;
+                case 'lg':
+                    insert = '/c_fit,h_90/f_auto';
+                    break;
+            }
+
+            $scope.url = $scope.url.slice(0, index) + insert + $scope.url.slice(index);
 
         }]
     };
